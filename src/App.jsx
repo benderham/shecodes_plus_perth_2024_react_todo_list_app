ea48c110ee1f7ea754bdc7f46ee765a12bfecbb3
Ben Derham <1805685+benderham@users.noreply.github.com>
2024-02-05 19:46:41 +0800

    step 6

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import TodoItem from './components/TodoItem';
+import TodoForm from './components/TodoForm';
 
 function App() {
   const [todos, setTodos] = useState([
@@ -8,13 +9,19 @@ function App() {
     'Buy Ben a beer',
   ]);
 
+  const addTodo = (text) => {
+    const newTodos = [...todos, text];
+    setTodos(newTodos);
+  };
+
   return (
-    <>
+    <div>
       <h1>todo list</h1>
       {todos.map((todo, index) => (
         <TodoItem key={index} text={todo} />
       ))}
-    </>
+      <TodoForm addTodo={addTodo} />
+    </div>
   );
 }
 
---
+++ src/components/TodoForm/index.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react';
+
+function TodoForm(props) {
+  const [value, setValue] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (!value) return;
+    props.addTodo(value);
+    setValue('');
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Add a todo..."
+        value={value}
+        onChange={(event) => setValue(event.target.value)}
+      />
+    </form>
+  );
+}
+
+export default TodoForm;

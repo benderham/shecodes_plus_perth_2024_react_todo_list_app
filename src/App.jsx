efeab3b57c36eb7cfcac1a601c509e0421a23e11
Ben Derham <1805685+benderham@users.noreply.github.com>
2024-02-08 17:15:35 +0800

    steps 7 through 9 and challenge

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,52 @@
 import { useState } from 'react';
 import TodoItem from './components/TodoItem';
 import TodoForm from './components/TodoForm';
+import './App.css';
 
 function App() {
   const [todos, setTodos] = useState([
-    'Walk the doggos',
-    'Feed the doggos',
-    'Buy Ben a beer',
+    {
+      text: 'Learn about React',
+      isCompleted: false,
+    },
+    {
+      text: 'Meet friend for lunch',
+      isCompleted: false,
+    },
+    {
+      text: 'Build really cool todo app',
+      isCompleted: false,
+    },
   ]);
 
   const addTodo = (text) => {
-    const newTodos = [...todos, text];
+    const newTodos = [...todos, { text }];
+    setTodos(newTodos);
+  };
+
+  const completeTodo = (index) => {
+    const newTodos = [...todos];
+    newTodos[index].isCompleted = true;
+    setTodos(newTodos);
+  };
+
+  const removeTodo = (index) => {
+    const newTodos = [...todos];
+    newTodos.splice(index, 1);
     setTodos(newTodos);
   };
 
   return (
-    <div>
-      <h1>todo list</h1>
+    <div className="app">
+      <h1 className="todo-list">todo list</h1>
       {todos.map((todo, index) => (
-        <TodoItem key={index} text={todo} />
+        <TodoItem
+          key={index}
+          todo={todo}
+          completeTodo={completeTodo}
+          removeTodo={() => removeTodo(index)}
+          index={index}
+        />
       ))}
       <TodoForm addTodo={addTodo} />
     </div>
--- src/components/TodoItem/index.jsx
+++ src/components/TodoItem/index.jsx
@@ -1,5 +1,17 @@
+import './style.css';
+
 function TodoItem(props) {
-  return <div>{props.text}</div>;
+  return (
+    <div className={`todo ${props.todo.isCompleted ? 'complete' : ''}`}>
+      {props.todo.text}
+      <div>
+        <button onClick={() => props.completeTodo(props.index)}>
+          Complete
+        </button>
+        <button onClick={() => props.removeTodo(props.index)}>X</button>
+      </div>
+    </div>
+  );
 }
 
 export default TodoItem;
